3a2c08124ebba3123df631734d32df43484b0111
Petrov-Oleksandr <amfitrion2011@gmail.com>
2022-08-01 18:11:15 +0300

    fourthcommit

--- Lesson01/Program.cs
+++ Lesson01/Program.cs
@@ -1,3 +1,6 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("How are you?");;
-Console.WriteLine("I am good");;
+Console.WriteLine("I am good");; 
+Console.WriteLine("Homework");; 
+Console.WriteLine("OK");; 
+
